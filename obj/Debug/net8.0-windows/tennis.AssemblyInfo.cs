35952f7bfa8e847f943287488aae529e2004a4de
TSIALONINA <149304848+Tsialone@users.noreply.github.com>
2025-01-07 23:44:32 +0300

    Suppresion des form1 et formdesigner

--- obj/Debug/net8.0-windows/tennis.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/tennis.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tennis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5af537aff9155e9d12350755159314d81505d43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+269b9232b99a447eb85ffbb1332a11aa2b4a11a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("tennis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tennis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
